65a3977e6ec2320b898974603f514d856609556c
Luis Perez <osorioperezluisfernando@gmail.com>
2025-03-11 15:12:35 -0500

    Integración del servicio para registro de nuevos usuarios y entidades. Actualizacion de los estilos del formulario y endpoint para registro de los nuevops usuarios.

--- client/src/api/requestAdmin/registerService.js
+++ client/src/api/requestAdmin/registerService.js
@@ -1,18 +1,41 @@
 import { api } from "../apiRest";
-export const adduser = async (event) => {
-    event.preventDefault();
-    const nit = event.target.nit.value;  
-    const rol = event.target.rol.value;
-    const pass = event.target.pass.value;
-    const passcon = event.target.passcon.value;
+export const adduser = async (formValues) => {
     try {
-    const response = await api.post('/adduser/newUser', {
-        nit, rol,
-        pass, passcon
+    const response = await api.post('/userManagement/addUsers', {
+        identificacion: formValues.nit,
+        rol: formValues.rol
     });
     return response.data;
 } catch (error) {
     return [];
 }
 };
+
+export const addEntity = async (payload) => {
+    try {
+        const response = await api.post('/userManagement/addEntity', {
+            identificacion: payload.nit,
+            nombre: payload.nombre,
+            correo: payload.correo
+        });
+        return response.data;
+    } catch (error) {
+        return [];
+    }
+};
+
+export const automaticRegistration = async (event, payload) => {
+    event.preventDefault();
+    const { identificacion, rol, password, ter_cond } = payload;
+    try {
+      await api.post("/userManagement/automaticRegistration", {
+        identificacion,
+        rol,
+        password,
+        ter_cond,
+      });
+      } catch (error) {
+        return [];
+      }
+  };
 
---
+++ client/src/app/home/user/entities/registerEntitie.jsx
@@ -0,0 +1,79 @@
+"use client";
+import { addEntity } from "@/api/requestAdmin/registerService";
+import { queryEntities } from "@/api/requestUsers/queryUsers";
+import ModalContent from "@/components/common/modal-content";
+import useForm from "@/hooks/useForm";
+import ErrorMessage from "@/components/common/ErrorMessage";
+
+export default function RegisterEntity({ isOpen, closeOpen, onAddRecord }) {
+  const { formValues, message, handleChange, isValid } = useForm({
+    nit: "",
+    nombre: "",
+    correo: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const result = await addEntity(formValues);
+    if (result.status === 200) {
+      const updatedData = await queryEntities();
+      onAddRecord(updatedData);
+      closeOpen();
+    }
+  };
+
+  return (
+    <ModalContent modalTitle={"Registro de Entidades"} state={isOpen} closeModal={closeOpen}>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="card">
+          <label htmlFor="nit">Identificación:</label>
+          <input
+            type="number"
+            name="nit"
+            id="identificacion"
+            value={formValues.nit}
+            onChange={handleChange}
+            aria-required="true"
+            className="input-field"
+          />
+          <ErrorMessage message={message.nit} />
+        </div>
+        <div className="card">
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            type="text"
+            name="nombre"
+            id="nombre"
+            value={formValues.nombre}
+            onChange={handleChange}
+            aria-required="true"
+            className="input-field"
+          />
+          <ErrorMessage message={message.nombre} />
+        </div>
+        <div className="card">
+          <label htmlFor="correo">Correo:</label>
+          <input
+            type="text"
+            name="correo"
+            id="correo"
+            value={formValues.correo}
+            onChange={handleChange}
+            aria-required="true"
+            className="input-field"
+          />
+          <ErrorMessage message={message.correo} />
+        </div>
+
+        <div className="btn_butones">
+          <button type="button" className="btn_cancelar" onClick={() => closeOpen(false)}>
+            Cancelar
+          </button>
+          <button type="submit" className="btn_registrar" disabled={!isValid}>
+            Registrar
+          </button>
+        </div>
+      </form>
+    </ModalContent>
+  );
+}
